e5dcf7c7fece189f6a71da93558f8b0ce0f86a22
Jared Moody <jared@jetbuilt.com>
2023-07-27 15:25:25 -0700

    Only render if mountTarget is present
    In Jetbuilt, we currently inject CKEditor4 into the initialContent, which for some reason briefly removes the body tag that we also pass.
    
    To workaround this, check to see if the document body is present before returning it as the mountTarget, and skip rendering into the container if the mountTarget is not present.

--- src/Frame.jsx
+++ src/Frame.jsx
@@ -81,7 +81,12 @@ export default class Frame extends Component {
     if (this.props.mountTarget) {
       return doc.querySelector(this.props.mountTarget);
     }
-    return doc.body.children[0];
+
+    if(doc.body) {
+      return doc.body.children[0];
+    }
+
+    return null;
   }
 
   renderFrameContents() {
@@ -120,7 +125,10 @@ export default class Frame extends Component {
       const callback = initialRender ? this.props.contentDidMount : this.props.contentDidUpdate;
       const mountTarget = this.getMountTarget();
 
-      ReactDOM.unstable_renderSubtreeIntoContainer(this, contents, mountTarget, callback);
+      if (mountTarget) {
+        ReactDOM.unstable_renderSubtreeIntoContainer(this, contents, mountTarget, callback);
+      }
+
       resetWarnings();
     } else {
       setTimeout(this.renderFrameContents.bind(this), 0);
